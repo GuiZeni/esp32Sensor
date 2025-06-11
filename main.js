a436ba2d1c39130763c80f1158fe226f21bdced9
Guilherme Zeni <163216154+GuiZeni@users.noreply.github.com>
2025-06-11 14:01:41 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 const accountSid = 'ACb32c05703c2627fe10edccd6db18d682';
-const authToken = '[AuthToken]';
+const authToken = '38e55a71e39c56c1f089fff9da7526b4';
 const client = require('twilio')(accountSid, authToken);
 
 // Rota que o ESP32 irá chamar
@@ -11,9 +11,9 @@ app.get('/movimento', (req, res) => {
   client.messages
     .create({
       from: 'whatsapp:+14155238886', // número sandbox Twilio
-      contentSid: 'HXb5b62575e6e4ff6129ad7c8efe1f983e', // seu template
-      contentVariables: '{"1":"12/1","2":"3pm"}',        // substituições do template
-      to: 'whatsapp:+554598431884' // seu número de WhatsApp
+      to: 'whatsapp:+554598431884', // seu número de WhatsApp
+      body: `🚨 Movimento detectado às ${new Date().toLocaleTimeString()}!`
+
     })
     .then(message => {
       console.log('Mensagem enviada. SID:', message.sid);
